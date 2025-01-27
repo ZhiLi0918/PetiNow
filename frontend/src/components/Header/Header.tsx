86cb18280c9f8170358bd88b22f8e16868f77e3a
ZhiLi0918 <zhili0918@gmail.com>
2025-01-27 00:04:53 -0500

    add initial home page

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import './header.css';
+
+export default function Header() {
+   return (
+      <div className='header'>
+         <div className='header-tab'>Home</div>
+         <div className='header-tab'>Report Lost</div>
+         <div className='header-tab'>Report Found</div>
+         <div className='header-tab'>FAQ</div>
+         <div className='header-tab'>Sucess Stories</div>
+         <div className='header-tab'>Media</div>
+      </div>
+   )
+}
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
+import Header from "../../components/Header/Header"
 import { logoutSuccess } from "../../redux/authSlice"
 import Button from "../../utils/Button"
 import { useDispatch } from "react-redux"
+import './home.css'
 
 export default function Home() {
    const dispatch = useDispatch()
@@ -12,9 +14,36 @@ export default function Home() {
       }
    }
    return (
-      <div>
-         <Button color={"#D5B893"} text={"Logout"} handler={handleLogOut} />
-         Home
-      </div>
+      <>
+         <Header />
+
+         <div className="home-main">
+            <div className="home-left-section">
+               <div className="home-left-wrapper">
+                  <div>
+                     <img src="/circle-logo.png" alt="" className="home-logo-circle"/>
+                  </div>
+
+                  <div className="home-slogan-wrapper">
+                     <div className="home-slogan-text"><span>Reuniting Pets,</span></div>
+                     <div className="home-slogan-text"><span>Restoring Smiles</span></div>
+                  </div>
+
+                  <div className="home-buttons-wrapper">
+                     <button>Report Lost</button>
+                     <button>Report Found</button>
+                  </div>
+               </div>
+            </div>
+
+            <div className="home-right-section">
+               <img src="/Home-Logo.png" alt="" />
+            </div>
+         </div>
+         {/* <div>
+            <Button color={"#D5B893"} text={"Logout"} handler={handleLogOut} />
+            Home
+         </div> */}
+      </>
    )
 }
